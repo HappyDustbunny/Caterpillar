765f0f012accc6847937baa43fce0a5ae2ae6d45
Mads Horn <madshorn@operamail.com>
2017-12-06 16:17:27 +0100

    Moved code from key catcher to main
    ... and adjusted the helmet

--- caterpillar.py
+++ caterpillar.py
@@ -2,15 +2,12 @@
 
 from vpython import box, sphere, color, scene, vector, compound, sleep, cross, radians
 
-forward = vector(1, 0, 0)
-up = vector(0, 1, 0)
-turn = False
-turn_axis = vector(0, 1, 0)
+keyevent = ''
 
 def make_head():
     ''' Make caterpillar head '''
     head_ball = sphere(color=color.orange)
-    helmet = sphere(pos=vector(.5, 0, 0), radius=1.55, opacity=0.3)
+    helmet = sphere(pos=vector(.1, 0.15, 0), radius=1.2, opacity=0.3)
     left_eye = sphere(pos=vector(0.5, 0.5, -0.4), radius=0.35)
     right_eye = sphere(pos=vector(0.5, 0.5, 0.4), radius=0.35)
     left_pupil = sphere(pos=vector(0.6, 0.56, -0.42), radius=0.25, color=color.black)
@@ -33,60 +30,63 @@ def make_body(caterpillar_pos):
 
 def direction(event):
     ''' Capture keyboard inupt and choose new direction and new orientation '''
-    value = event.key
-    global forward
-    global up
-    global turn
-    global turn_axis
-    if value == 'a':
-        forward = -cross(forward, up)
-        turn = True
-        turn_axis = up
-        print(forward, turn, turn_axis, 'a')
-    if value == 'd':
-        forward = cross(forward, up)
-        turn = True
-        turn_axis = -up
-        print(forward, turn, turn_axis, 'd')
-    if value == 'w':
-        forward, up = up, -forward
-        turn = True
-        turn_axis = cross(forward, up)
-        print(forward, turn, turn_axis, 'w')
-    if value == 's':
-        forward, up = -up, forward
-        turn = True
-        turn_axis = -cross(forward, up)
-        print(forward, turn, turn_axis, 's')
+    # value = event.key
+    global keyevent
+    keyevent = event.key
+
+
 
 def main():
     ''' Main loop '''
     scene.bind('keydown', direction)
+
     caterpillar_pos = []
     for dummy in range(5):
         caterpillar_pos.append(vector(-dummy, 0, 0))
+
     head = make_head()
     body = make_body(caterpillar_pos)
-    global forward
-    global up
-    global turn
-    global turn_axis
 
+    forward = vector(1, 0, 0)
+    up = vector(0, 1, 0)
+    turn = False
+    turn_axis = vector(0, 1, 0)
+
+    '''Listening for key presses'''
+    global keyevent
+
+    
     d_t = 0.4
 
     while True:
+        if keyevent == 'a':
+            forward = -cross(forward, up)
+            turn = True
+            turn_axis = up
+        if keyevent == 'd':
+            forward = cross(forward, up)
+            turn = True
+            turn_axis = -up
+        if keyevent == 'w':
+            forward, up = up, -forward
+            turn = True
+            turn_axis = cross(forward, up)
+        if keyevent == 's':
+            forward, up = -up, forward
+            turn = True
+            turn_axis = -cross(forward, up)
+        keyevent = ''
+
         old_caterpillar_pos = caterpillar_pos[:]
         caterpillar_pos[0] += forward
         head.pos = caterpillar_pos[0]
         # sleep(d_t)
         if turn:
             head.rotate(angle=radians(90), axis=turn_axis)
-            print('Head turned')
         for num, segment in enumerate(body):
             segment.pos = old_caterpillar_pos[num]
             if turn:
                 segment.rotate(angle=radians(90), axis=turn_axis)
-                print('Segment turned')
             caterpillar_pos[num + 1] = old_caterpillar_pos[num]
             # sleep(d_t)
         turn = False
