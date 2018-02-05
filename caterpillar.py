6015bd7ec8d832a97a08bbc97c67f63d5b790573
etok414 <etok414@gmail.com>
2018-02-05 11:30:45 +0100

    Small changes to planet_reached... (reverted from commit 382aab6b0f4f9b4e5b2059726a52a109bde02919)

--- caterpillar.py
+++ caterpillar.py
@@ -31,7 +31,7 @@ def space_direction(forward, upward):
     key_event = ''
     return forward, upward
 
-def planet_direction(forward, upward, on_planet, planets):
+def planet_direction(forward, upward, on_planet, planets, suit, targetfood):
     """ Checking key_event value and update direction while on planet. """
     global key_event
     p_shift = False
@@ -45,7 +45,11 @@ def planet_direction(forward, upward, on_planet, planets):
         forward = forward.rotate(1/planets[on_planet].radius, -cross(forward, upward))
     if key_event == 'w':  # Leaving planet
         forward, upward = upward, -forward
-        p_shift = True
+        if targetfood < len(planets[on_planet].food):
+            foodorder(planets, on_planet)
+        on_planet = -1  # -1 represents when the caterpillar isn't on a planet
+        for segment in suit:
+            segment.visible = True
     key_event = ''
     return forward, upward, p_shift
 
